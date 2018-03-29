e349d9b0358bc3ef69c33acc2032f88d159fcc5f
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-29 21:01:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,10 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
- if position_taken?(board, index) == true
-    return false
-  end
- 
-  if index.between?(0,8)
-    return true
-  end
+  isWithinBoard = index.between?(0, 8)
+  positionNotTaken = !position_taken?(board, index)
   
-  
-
-
+  return isWithinBoard && positionNotTaken
 end 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
